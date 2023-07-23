fa5da41ccf44334296e71ec82058d0399ab1202a
Martin Ferreira <martin.f.yic@gmail.com>
2023-07-23 20:04:37 -0300

    refactor: :lipstick: implement PokemonList.tsx into page/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,18 @@
 import { GetStaticProps, NextPage } from 'next';
-import { Layout } from '@/components/layouts';
+import { Pokemon, PokemonListResponse } from '@/interfaces';
 import { pokeApi } from '@/api';
-import { PokemonListResponse } from '@/interfaces';
+import { Layout } from '@/components/layouts';
+import { PokemonList } from '@/components/pokemon';
 
-const HomePage: NextPage = props => {
-	console.log(props);
+interface Props {
+	pokemons: Pokemon[];
+}
+
+const HomePage: NextPage<Props> = ({ pokemons }) => {
 	return (
 		<>
 			<Layout title='Pokemon list | NextJs App'>
-				<ul>
-					<li></li>
-				</ul>
+				<PokemonList pokemons={pokemons} />
 			</Layout>
 		</>
 	);
@@ -18,8 +20,19 @@ const HomePage: NextPage = props => {
 export const getStaticProps: GetStaticProps = async context => {
 	const { data } = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151');
 
+	// URL svg pokemon
+	// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/{pokemonId}.svg
+
+	const pokemons: Pokemon[] = data.results.map((poke, id) => ({
+		...poke,
+		id: id + 1,
+		img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
+			id + 1
+		}.svg`,
+	}));
+
 	return {
-		props: { pokemons: data.results },
+		props: { pokemons },
 	};
 };
 
