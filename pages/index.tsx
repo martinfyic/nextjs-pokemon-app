07fa6ec8b104b31432db1889f51d083044f2304a
Martin Ferreira <martin.f.yic@gmail.com>
2023-07-23 18:28:52 -0300

    feat: :zap: start implement getStaticProps page/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,26 @@
-import { Button } from '@nextui-org/react';
-import Head from 'next/head';
+import { GetStaticProps, NextPage } from 'next';
+import { Layout } from '@/components/layouts';
+import { pokeApi } from '@/api';
+import { PokemonListResponse } from '@/interfaces';
 
-const HomePage = () => {
+const HomePage: NextPage = props => {
+	console.log(props);
 	return (
 		<>
-			<Head>
-				<title>Pokemon App</title>
-			</Head>
-			<h1>Hola Mundo</h1>
-			<Button color='gradient'>Pokemon</Button>
+			<Layout title='Pokemon list | NextJs App'>
+				<ul>
+					<li></li>
+				</ul>
+			</Layout>
 		</>
 	);
+};
+export const getStaticProps: GetStaticProps = async context => {
+	const { data } = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151');
+
+	return {
+		props: { pokemons: data.results },
+	};
 };
 
 export default HomePage;
