92068a159855521b8d9d926d6982bc3f9a67c336
Martin Ferreira <martin.f.yic@gmail.com>
2023-07-23 22:37:46 -0300

    feat: :sparkles: add pages/favorites/index.tsx

---
+++ pages/favorites/index.tsx
@@ -0,0 +1,11 @@
+import { Layout } from '@/components/layouts';
+
+const FavoritesPage = () => {
+	return (
+		<Layout title='Favorites Pokemons | NextJs App'>
+			<h1>Favoritos</h1>
+		</Layout>
+	);
+};
+
+export default FavoritesPage;
