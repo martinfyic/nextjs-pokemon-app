2f708e813b67ba43328956fb12bbf35a0fdf676d
Martin Ferreira <martin.f.yic@gmail.com>
2023-07-27 21:51:42 -0300

    feat: :lipstick: add components/pokemon/FavoritesPokemons.tsx

---
+++ components/pokemon/FavoritesPokemons.tsx
@@ -0,0 +1,24 @@
+import { FC } from 'react';
+import { Grid } from '@nextui-org/react';
+import { FavoriteCardPokemon } from './';
+
+interface Props {
+	pokemons: number[];
+}
+
+export const FavoritesPokemons: FC<Props> = ({ pokemons }) => {
+	return (
+		<Grid.Container
+			gap={2}
+			direction='row'
+			justify='flex-start'
+		>
+			{pokemons.map(id => (
+				<FavoriteCardPokemon
+					key={id}
+					idPokemon={id}
+				/>
+			))}
+		</Grid.Container>
+	);
+};
