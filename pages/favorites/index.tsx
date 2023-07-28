be4d838e90d10a19f084ae01b6f2119826fd9d33
Martin Ferreira <martin.f.yic@gmail.com>
2023-07-27 21:49:38 -0300

    refactor: :zap: implement logic for empty or not empty pokemonfavorite

--- pages/favorites/index.tsx
+++ pages/favorites/index.tsx
@@ -1,10 +1,23 @@
+import { useEffect, useState } from 'react';
 import { Layout } from '@/components/layouts';
 import { EmptyFavorites } from '@/components/ui';
+import { localStorageFavorites } from '@/helpers';
+import { FavoritesPokemons } from '@/components/pokemon';
 
 const FavoritesPage = () => {
+	const [favritePokemons, setFavritePokemons] = useState<number[]>([]);
+
+	useEffect(() => {
+		setFavritePokemons(localStorageFavorites.pokemons());
+	}, []);
+
 	return (
 		<Layout title='Favorites Pokemons | NextJs App'>
-			<EmptyFavorites />
+			{favritePokemons.length === 0 ? (
+				<EmptyFavorites />
+			) : (
+				<FavoritesPokemons pokemons={favritePokemons} />
+			)}
 		</Layout>
 	);
 };
