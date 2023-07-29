7de9f3faf2faa524124cc906a42df7969717926f
Martin Ferreira <martin.f.yic@gmail.com>
2023-07-28 21:35:33 -0300

    perf: :zap: Error handling, when there is an error I return null

--- helpers/getPokemonInfo.ts
+++ helpers/getPokemonInfo.ts
@@ -2,11 +2,17 @@ import { pokeApi } from '@/api';
 import { PokemonTotalInfo } from '@/interfaces';
 
 export const getPokemonInfo = async (nameOrId: string) => {
-	const { data } = await pokeApi.get<PokemonTotalInfo>(`/pokemon/${nameOrId}`);
+	try {
+		const { data } = await pokeApi.get<PokemonTotalInfo>(
+			`/pokemon/${nameOrId}`
+		);
 
-	return {
-		id: data.id,
-		name: data.name,
-		sprites: data.sprites,
-	};
+		return {
+			id: data.id,
+			name: data.name,
+			sprites: data.sprites,
+		};
+	} catch (error) {
+		return null;
+	}
 };
